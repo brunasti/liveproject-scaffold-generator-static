85b72a7fa796cce67d9722d383001ad4c433407c
Paolo Brunasti <paolo@brunasti.it>
2021-02-07 13:42:19 +0100

    Removed template management

--- static.go
+++ static.go
@@ -2,30 +2,20 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"log"
 	"net/http"
-	"time"
 )
-
-type Thread struct {
-	Id        int
-	Uuid      string
-	Topic     string
-	UserId    int
-	CreatedAt time.Time
-}
 
 func main() {
 	fmt.Println(appName + " ------------")
 
 	mux := http.NewServeMux()
-	files := http.FileServer(http.Dir("/public"))
-	mux.Handle("/static/", http.StripPrefix("/static/", files))
-
-	mux.HandleFunc("/", index)
 
 	mux.HandleFunc("/healthcheck", healthCheck)
+
+	files := http.FileServer(http.Dir("./public"))
+	fmt.Println("Static files directory : ", &files)
+	mux.Handle("/", files)
 
 	server := &http.Server{
 		Addr:    "0.0.0.0:8080",
@@ -48,27 +38,28 @@ func healthCheck(writer http.ResponseWriter, request *http.Request) {
 	}
 }
 
-func index(w http.ResponseWriter, r *http.Request) {
-	log.Printf("/ [%v]\n", r.URL.Path[1:])
-	files := []string{"templates/layout.html",
-		"templates/public.navbar.html",
-		"templates/index.html"}
-
-	templateFiles, err := template.ParseFiles(files...)
-	if err != nil {
-		log.Printf("/ [%v] - Error retrieving files [%v]\n", r.URL.Path[1:], err)
-		return
-	}
-	templates := template.Must(templateFiles, err)
-
-	// TODO Create content
-	var threads = [1]Thread{}
-
-	err = templates.ExecuteTemplate(w, "layout", threads)
-
-	if err != nil {
-		log.Printf("/ [%v] - Error executing template [%v]\n", r.URL.Path[1:], err)
-		return
-	}
-
-}
+//func index(w http.ResponseWriter, r *http.Request) {
+//	log.Printf("/ [%v]\n", r.URL.Path[1:])
+//
+//	//files := []string{"templates/layout.html",
+//	//	"templates/public.navbar.html",
+//	//	"templates/index.html"}
+//	//
+//	//templateFiles, err := template.ParseFiles(files...)
+//	//if err != nil {
+//	//	log.Printf("/ [%v] - Error retrieving files [%v]\n", r.URL.Path[1:], err)
+//	//	return
+//	//}
+//	//templates := template.Must(templateFiles, err)
+//	//
+//	//// TODO Create content
+//	//var threads = [1]Thread{}
+//	//
+//	//err = templates.ExecuteTemplate(w, "layout", threads)
+//
+//	if err != nil {
+//		log.Printf("/ [%v] - Error providing file [%v]\n", r.URL.Path[1:], err)
+//		return
+//	}
+//
+//}
